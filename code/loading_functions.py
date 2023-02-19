8428e507c5e75575113f765e0781d3889a72b5de
Eric W <31462466+ewolf20@users.noreply.github.com>
2023-02-19 15:54:17 -0500

    Add function for programmatically updating central experiment parameters

--- code/loading_functions.py
+++ code/loading_functions.py
@@ -1,3 +1,4 @@
+import datetime
 import importlib.resources as pkg_resources
 import json
 import os
@@ -5,12 +6,36 @@ import os
 from .. import configs as c
 
 
+CENTRAL_EXPERIMENT_PARAMETERS_DATETIME_FORMAT_STRING = "%Y-%m-%d--%H-%M-%S"
+
+def _get_central_experiment_parameters_pathname_from_config():
+    IMAGE_SAVER_CONFIG_FILENAME = "image_saver_config_local.json"
+    with pkg_resources.path(c, IMAGE_SAVER_CONFIG_FILENAME) as config_path:
+        with open(config_path, 'r') as config_file:
+            config_dict = json.load(config_file) 
+            pathname = config_dict["experiment_parameters_pathname"]
+    return pathname
+
 def load_experiment_parameters_from_central_folder(pathname = None):
     if pathname is None:
-        IMAGE_SAVER_CONFIG_FILENAME = "image_saver_config_local.json"
-        with pkg_resources.path(c, IMAGE_SAVER_CONFIG_FILENAME) as config_path:
-            with open(config_path, 'r') as config_file:
-                config_dict = json.load(config_file) 
-                pathname = config_dict["experiment_parameters_pathname"]
+        pathname = _get_central_experiment_parameters_pathname_from_config()
     with open(pathname, 'r') as experiment_parameters_file:
         return json.load(experiment_parameters_file)
+
+
+
+def update_central_experiment_parameters(varname, val, add_var = False, pathname = None):
+    if pathname is None:
+        pathname = _get_central_experiment_parameters_pathname_from_config()
+    experiment_parameters_dict = load_experiment_parameters_from_central_folder(pathname = pathname)
+    experiment_parameters_values = experiment_parameters_dict["Values"]
+    experiment_parameters_update_times = experiment_parameters_dict["Update_Times"]
+    if add_var or varname in experiment_parameters_values:
+        experiment_parameters_values[varname] = val
+        current_datetime = datetime.datetime.now()
+        datetime_string = current_datetime.strftime(CENTRAL_EXPERIMENT_PARAMETERS_DATETIME_FORMAT_STRING)
+        experiment_parameters_update_times[varname] = datetime_string
+        with open(pathname, 'w') as experiment_parameters_file:
+            json.dump(experiment_parameters_dict, experiment_parameters_file)
+    else:
+        raise ValueError("The stipulated varname is not in the experiment parameters. Pass add_var=True to add new variables.")
--- tests/test_loading_functions.py
+++ tests/test_loading_functions.py
@@ -19,11 +19,43 @@ def test_load_experiment_parameters_from_central_folder():
     sample_loaded_dict = loading_functions.load_experiment_parameters_from_central_folder(
         os.path.join("resources", "experiment_parameters_sample.json")
     )
-    assert len(sample_loaded_dict) == 0
+    assert len(sample_loaded_dict) == 2
+    assert "Values" in sample_loaded_dict 
+    assert "Update_Times" in sample_loaded_dict
     true_loaded_dict = loading_functions.load_experiment_parameters_from_central_folder() 
     assert len(true_loaded_dict) == 2 
     assert "Values" in true_loaded_dict 
     assert "Update_Times" in true_loaded_dict
+
+
+def test_update_central_experiment_parameters():
+    sample_pathname = os.path.join("resources", "experiment_parameters_sample.json")
+    sample_temp_pathname = os.path.join("resources", "experiment_parameters_sample_temp.json")
+    try:
+        shutil.copyfile(sample_pathname, sample_temp_pathname)
+        sample_loaded_dict = loading_functions.load_experiment_parameters_from_central_folder(
+            pathname = sample_temp_pathname
+        )
+        assert len(sample_loaded_dict) == 2
+        assert len(sample_loaded_dict["Values"]) == 0
+        loading_functions.update_central_experiment_parameters("foo", 1, add_var = True, pathname = sample_temp_pathname)
+        sample_reloaded_dict = loading_functions.load_experiment_parameters_from_central_folder(
+            pathname = sample_temp_pathname
+        )
+        assert sample_reloaded_dict["Values"]["foo"] == 1 
+        try:
+            loading_functions.update_central_experiment_parameters("bar", 2, add_var = False, pathname = sample_temp_pathname)
+        except ValueError as e:
+            pass 
+        else:
+            assert False
+        loading_functions.update_central_experiment_parameters("foo", 2, pathname = sample_temp_pathname) 
+        sample_reloaded_dict = loading_functions.load_experiment_parameters_from_central_folder(
+            pathname = sample_temp_pathname
+        )
+        assert sample_reloaded_dict["Values"]["foo"] == 2
+    finally:
+        os.remove(sample_temp_pathname)
 
 
 
