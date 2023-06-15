b04eccf2568a36a34a36614f9ca8d14c85fa54c2
BEC1 <bec1mailbox@gmail.com>
2023-06-15 10:13:34 -0400

    Working script for DC guppy camera viewing

---
+++ scripts/guppy_viewer_script.py
@@ -0,0 +1,88 @@
+import sys 
+import os
+
+import numpy as np 
+from vimba import Vimba
+
+path_to_file = os.path.dirname(os.path.abspath(__file__))
+path_to_satyendra = path_to_file + "/../../"
+
+sys.path.insert(0, path_to_satyendra)
+
+from satyendra.code import plotting_utilities, guppy_cam_wrapper, loading_functions
+
+
+def main():
+    print("Initializing")
+    with Vimba.get_instance() as vimba:
+        camera_id, *cam_setup_params = parse_clas()
+        cams = vimba.get_all_cameras() 
+        matching_id_cams = [cam for cam in cams if cam.get_id() == camera_id]
+        fig, ax = plotting_utilities.initialize_live_plot()
+        with matching_id_cams[0] as cam:
+            cam_wrapper = guppy_cam_wrapper.GuppyCamWrapper(cam)
+            setup_camera(cam_wrapper, *cam_setup_params)
+            print("Done. Use Ctrl+C to exit.")
+            try:
+                cam_wrapper.start_streaming()
+                while True:
+                    current_frame = cam_wrapper.get_streamed_frame()
+                    if not current_frame is None:
+                        plotting_utilities.update_live_plot_imshow(current_frame, ax = ax, vmin = 0, vmax = 255, cmap="gray")
+            finally:
+                cam_wrapper.stop_streaming()
+
+
+def parse_clas():
+    command_line_args = sys.argv[1:]
+    if len(command_line_args) == 0 or command_line_args[0] == "help":
+        _help_function() 
+        exit(0)
+    camera_name = command_line_args[0]
+    camera_config_dict_dict = loading_functions.load_guppy_camera_parameters_json() 
+    camera_config_dict = camera_config_dict_dict[camera_name] 
+    camera_id = camera_config_dict["camera_id"]
+    if len(command_line_args) > 1:
+        exposure_time_us = float(command_line_args[1])
+    else:
+        exposure_time_us = camera_config_dict["default_exposure"]
+    if len(command_line_args) > 2:
+        exposure_auto = (command_line_args[2] == "True")
+    else:
+        exposure_auto = camera_config_dict["default_auto_exposure"]
+    if len(command_line_args) > 3:
+        image_height = int(command_line_args[3]) 
+    else:
+        image_height = camera_config_dict["default_image_height"] 
+    if len(command_line_args) > 4:
+        image_width = int(command_line_args[4])
+    else:
+        image_width = camera_config_dict["default_image_width"]
+    return (camera_id, exposure_time_us, exposure_auto, image_height, image_width) 
+
+
+
+def setup_camera(cam_wrapper, exposure_time_us, exposure_auto, image_height, image_width):
+    cam_wrapper.set_auto_exposure(exposure_auto)
+    cam_wrapper.set_exposure_time(exposure_time_us) 
+    cam_wrapper.set_image_height(image_height) 
+    cam_wrapper.set_image_width(image_width)
+
+
+
+
+
+def _help_function():
+    print("Program name: Guppy Live Viewer Script") 
+    print("Description: Convenience script for live viewing of the feed from a single guppy camera.") 
+    print("CLAs:")
+    print("Note: If CLAs are specified, they must be done in order. Optional values are initialized from the configuration JSON if not specified.")
+    print("1: Camera name. Options are those specified in guppy_camera_config_local.json.")
+    print("2: (optional) Exposure_time_us. The exposure time, in us.")
+    print("3: (optional) Exposure_auto. If 'True', the camera will be set to auto exposure. Any other input gives fixed exposure.")
+    print("4: (optional) Height. The image height, in pixels.")
+    print("5: (optional) Width. The image width, in pixels.")
+
+
+if __name__ == "__main__":
+    main()
