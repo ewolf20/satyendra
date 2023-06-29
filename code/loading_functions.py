0b5d0fa24b9ccb602be8cf01d9bfb23b3697bbcd
Eric W <31462466+ewolf20@users.noreply.github.com>
2023-06-29 18:52:59 -0400

    Remove duplicated code block - no change in functionality

--- code/loading_functions.py
+++ code/loading_functions.py
@@ -24,17 +24,6 @@ def _get_central_experiment_parameters_pathname():
             pathname = config_dict["experiment_parameters_pathname"]
             return pathname
 
-
-CENTRAL_EXPERIMENT_PARAMETERS_DATETIME_FORMAT_STRING = "%Y-%m-%d--%H-%M-%S"
-
-def _get_central_experiment_parameters_pathname_from_config():
-    IMAGE_SAVER_CONFIG_FILENAME = "image_saver_config_local.json"
-    with pkg_resources.path(c, IMAGE_SAVER_CONFIG_FILENAME) as config_path:
-        with open(config_path, 'r') as config_file:
-            config_dict = json.load(config_file) 
-            pathname = config_dict["experiment_parameters_pathname"]
-    return pathname
-
 def load_experiment_parameters_from_central_folder(pathname = None):
     if pathname is None:
         pathname = _get_central_experiment_parameters_pathname()
